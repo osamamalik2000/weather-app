156cbeeab765c4c13268a7aa735bf2f66e24fcab
Osama Malik <itsosamamalik@gmail.com>
2021-11-03 23:41:58 +0530

    First commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,15 +4,96 @@ import { FetchServiceService } from '../../services/fetch-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor(private ser: FetchServiceService) { 
-    this.ser.getlocation();
+  // This section is for handeling dates, days
+  days: any[] = [];
+  date = new Date(1970, 0, 1);
+  today: any;
+  todayDate:any;
+  // End of date sections
+  // For hadeling response
+  data: any;
+  forecast: any[] = [];
+  // Hide and show data if location info is present
+  visible = {
+    'display': 'none'
+  };
+  constructor(private ser: FetchServiceService) {}
+  ngOnInit() {
+    // this.getImage(5000);
+    this.getLocation();
   }
-
-  ngOnInit(): void {
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          this.ser
+            .getLocation(pos.coords.latitude, pos.coords.longitude)
+            .subscribe((res) => {
+              this.data = res;
+              this.forecast = this.data.daily.slice(0,6);
+              this.getTodayDate();
+              console.log(this.data);
+            });
+        },
+        (err) => {
+          console.log('Can not get current weather: ' + err);
+        }
+      );  
+    }
   }
+  // Handeling today and upcoming dates
+  getTodayDate(){
+    this.date.setSeconds(this.data.current.dt);
+    this.today = this.date.getDay();
+    switch(this.today){
+      case 0 :{
+        this.days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        break;
+      }
+      case 1:{
+        this.days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+        break;
+      }
+      case 2 :{
+        this.days = ['Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday','Monday'];
+        break;
+      }
+      case 3:{
+        this.days = ['Wednesday','Thursday','Friday','Saturday','Sunday','Monday','Tuesday',];
+        break;
+      }
+      case 4 :{
+        this.days = ['Thursday','Friday','Saturday','Sunday','Monday','Tuesday','Wednesday'];
+        break;
+      }
+      case 5:{
+        this.days = ['Friday','Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday'];
+        break;
+      }
+      case 6:{
+        this.days = ['Saturday','Sunday','Monday','Tuesday','Wednesday','Thursday','Friday'];
+        break;
+      }
+      default:{
+        this.days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        break;
+      }
+    }
+    this.todayDate = `${this.date.getDate()} ${this.date.toLocaleString('default', {month: 'long',})}`;
+  }
+  // Selecting images accroding to weather
+  // getImage(img: any){
+  //   let n = img.toString();
+  //   console.log(n[0]);
 
+  //   switch(n){
+  //     case '2':{
+  //       return 
+  //     }
+
+  //   }
+  // }
 }
--- src/app/services/fetch-service.service.ts
+++ src/app/services/fetch-service.service.ts
@@ -1,31 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class FetchServiceService {
   constructor(private http: HttpClient) {}
-  getlocation() {
-    console.log(navigator.geolocation.getCurrentPosition(success, error));
-    let lat, long;
-    function success(position: any) {
-      console.log(position.coords.latitude);
-      console.log(position.coords.longitude);
-      lat = position.coords.latitude;
-      long = position.coords.longitude;
-    }
-    if (navigator.geolocation) {
-      this.getCity(lat, long);
-    }
-    function error(err: any) {
-      console.log(err);
-    }
-  }
-  getCity(lat: any, long: any) {
-    let city = this.http.get(
-      `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=972dd6236d62fcedef347feb8d9e9b5d`
-    );
-    console.log('city: ', city);
+
+  APIkey = '972dd6236d62fcedef347feb8d9e9b5d';
+  // Metric unit system
+  getLocation(lat: any, lon: any): Observable<Object> {
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${this.APIkey}`
+    )
   }
 }
